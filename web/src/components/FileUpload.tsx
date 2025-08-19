2d522e82fe7d1187c957fec3ecf750fa736f5661
Asif Rahaman <asifrahaman162@gmail.com>
2025-08-19 22:46:15 +0530

    cleanup: cleanup and broke the telegram and gmail client

--- web/src/components/FileUpload.tsx
+++ web/src/components/FileUpload.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import { useState } from "react";
-import { backend_url } from "@/config/config";
+import axios from 'axios';
+import { useState } from 'react';
+import { backend_url } from '@/config/config';
 
 export default function FileUpload() {
   const [file, setFile] = useState<File | null>(null);
-  const [message, setMessage] = useState<string>("");
+  const [message, setMessage] = useState<string>('');
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -14,35 +14,29 @@ export default function FileUpload() {
     }
   };
 
+  const handleUpload = async () => {
+    if (!file) {
+      setMessage('Please select a file first.');
+      return;
+    }
 
-const handleUpload = async () => {
-  if (!file) {
-    setMessage("Please select a file first.");
-    return;
-  }
+    const formData = new FormData();
+    formData.append('file', file);
 
-  const formData = new FormData();
-  formData.append("file", file);
-
-  try {
+    try {
       const token = localStorage.getItem('org_jwt');
-    const res = await axios.post(
-      `${backend_url}/api/v1/uploads/upload-file/`,
-      formData,
-      {
+      const res = await axios.post(`${backend_url}/api/v1/uploads/upload-file/`, formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
-          "Authorization": `Bearer ${token}`
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
         },
-      }
-    );
+      });
 
-    setMessage(`✅ ${res.data.message}: ${res.data.filename}`);
-  } catch  {
-    console.error("Something went wrong while uploading the file.")
-  }
-};
-
+      setMessage(`✅ ${res.data.message}: ${res.data.filename}`);
+    } catch {
+      console.error('Something went wrong while uploading the file.');
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center  p-6 bg-gray-100">
@@ -59,9 +53,7 @@ const handleUpload = async () => {
         >
           Upload
         </button>
-        {message && (
-          <p className="mt-4 text-center text-sm text-gray-800">{message}</p>
-        )}
+        {message && <p className="mt-4 text-center text-sm text-gray-800">{message}</p>}
       </div>
     </div>
   );
